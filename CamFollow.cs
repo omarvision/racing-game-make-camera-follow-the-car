8534217449741cce1051b23adc00cf416d1c94b6
Omar Santiago <omarkenosia@gmail.com>
2021-07-12 14:43:30 -0400

    Add files via upload

---
+++ CamFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public GameObject Car = null;
+    public GameObject FollowObject = null;
+    public float FollowSpeed = 3.0f;
+
+    private void Update()
+    {
+        if (FollowObject == null || Car == null)
+        {
+            return;
+        }
+
+        this.transform.LookAt(Car.transform);
+        float CameraMoveAmount = Mathf.Abs(Vector3.Distance(this.transform.position, FollowObject.transform.position) * FollowSpeed);
+        this.transform.position = Vector3.MoveTowards(this.transform.position, FollowObject.transform.position, CameraMoveAmount * Time.deltaTime);
+    }
+}
